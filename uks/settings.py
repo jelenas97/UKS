a15c5dc428edf214b476216bacfb8d3a906e2305
syn-petarcurcin <petar.curcin1@synechron.com>
2021-01-26 21:08:51 +0100

    created app version control

--- uks/settings.py
+++ uks/settings.py
@@ -30,6 +30,7 @@ DEBUG = True
 # Application definition
 
 INSTALLED_APPS = [
+    'version_control.apps.VersionControlConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
