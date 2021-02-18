8aa7441b2efaf87705ae672ff627d7d2ee62e759
jelenas97 <jekibp@gmail.com>
2021-02-18 20:12:49 +0100

    Wiki update implemented

--- version_control/wiki/views.py
+++ version_control/wiki/views.py
@@ -5,7 +5,7 @@ from django.http import HttpResponseRedirect
 from django.shortcuts import get_object_or_404, render
 # Create your views here.
 from django.urls import reverse_lazy
-from django.views.generic import CreateView, ListView, DetailView
+from django.views.generic import CreateView, ListView, DetailView, UpdateView
 
 from version_control.models import Repository
 from version_control.projects.forms import WikiCreateForm
@@ -15,7 +15,7 @@ from version_control.wiki.models import Wiki, WikiRevision
 def saveWiki(request, repoId):
     repo = Repository.objects.get(id=repoId)
 
-    if request.method =='POST':
+    if request.method == 'POST':
         form = WikiCreateForm(request.POST)
 
         if form.is_valid():
@@ -33,17 +33,20 @@ def saveWiki(request, repoId):
     return render(request, "wiki/wiki_form.html", {'form': form})
 
 
-class WikiCreateView(LoginRequiredMixin, CreateView):
+class WikiUpdateView(LoginRequiredMixin, UpdateView):
     model = Wiki
     fields = ['title', 'content']
+    template_name = 'wiki/wiki_update.html'
 
     def form_valid(self, form):
-        form.instance.repository = get_object_or_404(Repository, id=self.kwargs['repoId'])
+        if form.is_valid():
+            wiki = Wiki.objects.get(id=self.kwargs['pk'])
+            wikiRevision = WikiRevision.objects.create(wiki_id=wiki.id, reviser_id=self.request.user.id)
+            wikiRevision.save()
         return super().form_valid(form)
 
     def get_success_url(self):
-        print(self.get_object())
-        return reverse_lazy('wiki-preview', kwargs={'repoId': self.object.repository.id})
+        return reverse_lazy('wiki-page-preview', kwargs={'repoId': self.object.repository.id, 'pk': self.kwargs['pk']})
 
 
 class WikiPreview(ListView):
@@ -52,6 +55,20 @@ class WikiPreview(ListView):
 
     def get_queryset(self):
         return Wiki.objects.filter(repository_id=self.kwargs['repoId'])
+
+    def get_context_data(self, **kwargs):
+        context = super(WikiPreview, self).get_context_data(**kwargs)
+        allWikis = Wiki.objects.filter(repository_id=self.kwargs['repoId'])
+        context['wikiRevisions'] = WikiRevision.objects.filter(wiki_id=allWikis[0].id)
+        return context
+
+
+class WikiRevisionsPreview(ListView):
+    template_name = 'wiki/wiki_revisions_preview.html'
+    context_object_name = 'wikiRevisions'
+
+    def get_queryset(self):
+        return WikiRevision.objects.filter(wiki_id=self.kwargs['pk'])
 
 
 class WikiPagePreview(DetailView):
@@ -64,5 +81,5 @@ class WikiPagePreview(DetailView):
     def get_context_data(self, **kwargs):
         context = super(WikiPagePreview, self).get_context_data(**kwargs)
         context['wikis'] = Wiki.objects.filter(repository_id=self.kwargs['repoId'])
-        context['wikiRevisions'] = WikiRevision.objects.filter(pk=self.kwargs['pk'])
+        context['wikiRevisions'] = WikiRevision.objects.filter(wiki_id=self.kwargs['pk'])
         return context
