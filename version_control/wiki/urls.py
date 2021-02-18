38b4c3a319c7b29a1c07d9762989230fe86c2334
jelenas97 <jekibp@gmail.com>
2021-02-18 20:12:49 +0100

    Wiki update implemented

--- version_control/wiki/urls.py
+++ version_control/wiki/urls.py
@@ -1,13 +1,15 @@
 from django.urls import path
 from .views import (
-    WikiCreateView,
     WikiPreview,
-    WikiPagePreview, saveWiki
+    WikiPagePreview, saveWiki, WikiRevisionsPreview, WikiUpdateView
 
 )
 
 urlpatterns = [
     path('wiki/new', saveWiki, name='wiki-create'),
     path('wiki/<int:pk>/', WikiPagePreview.as_view(), name='wiki-page-preview'),
-    path('wiki', WikiPreview.as_view(), name='wiki-preview')
+    path('wiki', WikiPreview.as_view(), name='wiki-preview'),
+    path('wiki/<int:pk>/revisions', WikiRevisionsPreview.as_view(), name='wiki-revisions-preview'),
+    path('wiki/<int:pk>/update', WikiUpdateView.as_view(), name='wiki-update'),
+
 ]
