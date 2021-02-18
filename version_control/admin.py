ae2a75735498b648984ded81b61b4eec0f3fb6c3
petarcurcin <petar.curcin97@gmail.com>
2021-02-18 14:43:58 +0100

    change model from appuser to profile

--- version_control/admin.py
+++ version_control/admin.py
@@ -8,15 +8,16 @@ from version_control.repository.models import Repository
 from version_control.organizations.models import Organization
 from version_control.milestones.models import Milestone
 from version_control.labels.models import Label
-from version_control.wiki.models import Wiki
 from version_control.tasks.models import Task
 from version_control.tasks.models import TaskRevision
+from version_control.wiki.models import Wiki, WikiRevision
 
 
 admin.site.register(Project)
 admin.site.register(Repository)
 admin.site.register(Label)
 admin.site.register(Wiki)
+admin.site.register(WikiRevision)
 admin.site.register(AppUser)
 admin.site.register(Organization)
 admin.site.register(Milestone)
