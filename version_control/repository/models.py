2b39686f8ebd06c91fa12faf4d629bc6e15eaead
petarcurcin <57414071+petarcurcin@users.noreply.github.com>
2021-02-19 18:16:47 +0100

    Merge branch 'development' of https://github.com/jelenas97/UKS into feature/connect-with-github-repo (#35)
    * added owner of repository in model
    
    * get data from real git repository

--- version_control/repository/models.py
+++ version_control/repository/models.py
@@ -8,7 +8,8 @@ class Repository(models.Model):
     name = models.CharField(max_length=50)
     description = models.TextField()
     isPrivate = models.BooleanField()
-    contributors = models.ManyToManyField(AppUser)
+    contributors = models.ManyToManyField(AppUser, related_name = 'contributors')
+    owner = models.ForeignKey(AppUser, on_delete=models.CASCADE, related_name = 'owner', null = True)
 
     def get_absolute_url(self):
         return reverse('repository-detail', kwargs={'pk': self.pk})
