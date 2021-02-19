4fa6ead81d4aeaf1598fd080bde96dd09b744966
petarcurcin <petar.curcin97@gmail.com>
2021-02-19 16:44:09 +0100

    get data from real git repository

--- version_control/repository/urls.py
+++ version_control/repository/urls.py
@@ -17,6 +17,8 @@ urlpatterns = [
     path('repository/new/', RepositoryCreateView.as_view(), name='repository-create'),
     path('repository/<int:pk>/update/', RepositoryUpdateView.as_view(), name='repository-update'),
     path('repository/<int:pk>/delete/', RepositoryDeleteView.as_view(), name='repository-delete'),
+    path('repository/<int:repoId>/realGithub/', views.get_real_github_data, name='real-github'),
+    path('repository/<int:repoId>/realGithub/branch/<path:branch>/commits/', views.get_commits_from_branch, name='real-branch-commits'),
 
     path('repository/<int:repoId>/', include('version_control.labels.urls')),
 
